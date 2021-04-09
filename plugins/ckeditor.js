4d42f273458861c4f8807a711ea0b0802837c993
liujianzeng <1623685118@qq.com>
2021-04-09 16:10:40 +0800

    save

--- plugins/ckeditor.js
+++ plugins/ckeditor.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import CKEditor from '@ckeditor/ckeditor5-vue'
+import CKEditor from '@ckeditor/ckeditor5-vue2'
 import "@ckeditor/ckeditor5-build-decoupled-document/build/translations/zh-cn";
 Vue.use(CKEditor);
 
